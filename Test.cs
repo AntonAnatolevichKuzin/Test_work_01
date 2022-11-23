124314585ad2a4c414170560aa7e3cf4aaf576d3
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-11-23 19:39:10 +0500

    Added method "SortArray"

--- Test.cs
+++ Test.cs
@@ -1,6 +1,6 @@
 string[] array = {"if", "for", "else", "{", "<", "while"};
 PrintArray(array);
-
+SortArray(array);
 
 void PrintArray(string[] array)
 {
@@ -13,3 +13,20 @@ void PrintArray(string[] array)
     
  }
 
+void SortArray(string[] array)
+{
+    Console.Write("-> [");
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string temp = array[i];
+        int length = temp.Length;
+        
+        if (length <=2)
+        {
+            if (count > 0) Console.Write(", ");
+        Console.Write($"{array[i]}"); count++;
+        }
+    }
+    Console.Write("]");
+}
